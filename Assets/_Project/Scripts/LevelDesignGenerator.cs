fe9a2ad57f09673c69c6d8809058fce37b5bf7fe
Sreden <maxime.godet10@gmail.com>
2024-11-21 13:40:57 +0100

    portal generation

--- Assets/_Project/Scripts/LevelDesignGenerator.cs
+++ Assets/_Project/Scripts/LevelDesignGenerator.cs
@@ -1,57 +1,87 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LevelDesignGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject player;
-
     [SerializeField] private GameObject groundPrefab;
-    [SerializeField] private GameObject portalsPrefab;
-    [SerializeField] private GameObject[] environmentPrefabs;
+    [SerializeField] private GameObject portalPrefab;
 
     [SerializeField] private int initialGrounds = 5;
     [SerializeField] private float groundLength = 20f;
 
-    private Queue<GameObject> activeGrounds = new();
-    private Vector3 nextSpawnPosition;
+    private Queue<GameObject> portals = new();
+    private Queue<GameObject> grounds = new();
+
+    private Vector3 groundSpawnPosition;
+    private Vector3 portalSpawnPosition;
+
+    public enum LD_Type
+    {
+        GROUND,
+        PORTAL
+    }
 
     void Start()
     {
-        nextSpawnPosition = transform.position;
-
+        groundSpawnPosition = transform.position;
         for (int i = 0; i < initialGrounds; i++)
         {
-            SpawnSegment();
+            SpawnLD(LD_Type.GROUND, ref groundSpawnPosition, groundLength);
         }
+
+        portalSpawnPosition = transform.position + (player.transform.position.z + (initialGrounds - 1) * groundLength) * Vector3.forward;
+
+        SpawnLD(LD_Type.PORTAL, ref portalSpawnPosition, 0);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (activeGrounds.Count == 0)
-        {
-            return;
-        }
+        portalSpawnPosition = transform.position + (player.transform.position.z + (initialGrounds - 1) * groundLength) * Vector3.forward;
 
-        if (player.transform.position.z > nextSpawnPosition.z - ((initialGrounds - 1) * groundLength))
+        if (player.transform.position.z > groundSpawnPosition.z - ((initialGrounds - 1) * groundLength))
         {
-            SpawnSegment();
-            RemoveOldSegment();
+            SpawnLD(LD_Type.GROUND, RemoveOldLD(LD_Type.GROUND), ref groundSpawnPosition, groundLength); // simple pooling
         }
     }
 
-    void SpawnSegment()
+    void SpawnLD(LD_Type type, ref Vector3 spawnPosition, float nextForwardOffset)
     {
-        GameObject newSegment = Instantiate(groundPrefab, nextSpawnPosition, Quaternion.identity);
-        activeGrounds.Enqueue(newSegment);
-        nextSpawnPosition += Vector3.forward * groundLength;
+        GameObject newSegment = Instantiate(GetPrefabWithType(type), spawnPosition, Quaternion.identity);
+        GetPrefabQueueWithType(type).Enqueue(newSegment);
+        spawnPosition += Vector3.forward * nextForwardOffset;
     }
 
-    private void RemoveOldSegment()
+    void SpawnLD(LD_Type type, GameObject alreadySpawnedLD, ref Vector3 spawnPosition, float nextForwardOffset)
     {
-        GameObject oldSegment = activeGrounds.Dequeue();
-        Destroy(oldSegment);
+        alreadySpawnedLD.transform.position = spawnPosition;
+        GetPrefabQueueWithType(type).Enqueue(alreadySpawnedLD);
+        spawnPosition += Vector3.forward * nextForwardOffset;
+    }
+
+    private GameObject RemoveOldLD(LD_Type type)
+    {
+        return GetPrefabQueueWithType(type).Dequeue();
+    }
+
+    private Queue<GameObject> GetPrefabQueueWithType(LD_Type Type)
+    {
+        return Type switch
+        {
+            LD_Type.GROUND => grounds,
+            LD_Type.PORTAL => portals,
+            _ => grounds,
+        };
+    }
+
+    private GameObject GetPrefabWithType(LD_Type Type)
+    {
+        return Type switch
+        {
+            LD_Type.GROUND => groundPrefab,
+            LD_Type.PORTAL => portalPrefab,
+            _ => groundPrefab,
+        };
     }
 }
