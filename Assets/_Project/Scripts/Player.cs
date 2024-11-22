a47326dd062d569baa5ec292074106373993386b
Maxime Godet <47150550+Sreden@users.noreply.github.com>
2024-11-22 17:52:52 +0100

    event ready - speed up (#5)

--- Assets/_Project/Scripts/Player.cs
+++ Assets/_Project/Scripts/Player.cs
@@ -1,35 +1,65 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-[RequireComponent(typeof(InputListener), typeof(MoveForward))]
+[RequireComponent(typeof(InputListener), typeof(MoveForward), typeof(Rigidbody))]
 public class Player : Singleton<Player>
 {
     [SerializeField] private ColorModifier colorModifier;
     private InputListener inputs;
     private MoveForward moveForward;
+    private Rigidbody rb;
 
     public event Action<Color> OnColorSwap;
 
     protected override void Awake()
     {
         base.Awake();
-        
         inputs = GetComponent<InputListener>();
         moveForward = GetComponent<MoveForward>();
+        moveForward.IsActive = false;
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+
         inputs.OnInteract += Inputs_OnInteract;
+
+        if (GameManager.Instance == null)
+        {
+            Instance_OnReady();
+        }
+        else
+        {
+            GameManager.Instance.OnReady += Instance_OnReady;
+        }
+    }
+
+    private void Instance_OnReady()
+    {
+        moveForward.IsActive = true;
     }
 
     private void Start()
     {
         colorModifier.SwapColor();
         OnColorSwap?.Invoke(colorModifier.CurrentColor);
+    }
 
-        
+    private void OnDestroy()
+    {
+        if (GameManager.Instance == null)
+        {
+            return;
+        }
+
+        GameManager.Instance.OnReady -= Instance_OnReady;
     }
 
     private void Update()
     {
-        GameManager.Instance.SetScore(transform.position.z);
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.SetScore(transform.position.z);
+        }
     }
 
     private void Inputs_OnInteract()
@@ -45,14 +75,17 @@ public class Player : Singleton<Player>
         {
             // Compare player color with portal color
             Color portalColor = wall.GetPortalColor();
-            if (ColorModifier.ColorsAreEqual(colorModifier.CurrentColor, portalColor))
-            {
-                moveForward.Speed += 0.5f;
-            }
-            else
+            if (!ColorModifier.ColorsAreEqual(colorModifier.CurrentColor, portalColor))
             {
                 Handheld.Vibrate();
-                GameManager.Instance.Play();
+                if (GameManager.Instance != null)
+                {
+                    GameManager.Instance.Play();
+                }
+                else
+                {
+                    SceneManager.LoadScene("MainScene");
+                }
             }
         }
     }
